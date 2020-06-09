b586434a7219e668ef5574f73380eaf1fd6678cb
Evan Tey <evantey14@gmail.com>
2020-06-09 18:16:46 -0400

    Reformat views

--- confusion/views.py
+++ confusion/views.py
@@ -4,11 +4,7 @@ def index(request):
     return render(request, 'confusion/index.html')
 
 def student(request, room_name):
-    return render(request, 'confusion/student.html', {
-        'room_name': room_name
-    })
+    return render(request, 'confusion/student.html', {'room_name': room_name})
 
 def teacher(request, room_name):
-    return render(request, 'confusion/teacher.html', {
-        'room_name': room_name
-    })
+    return render(request, 'confusion/teacher.html', {'room_name': room_name})
